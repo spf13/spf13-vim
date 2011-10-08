8d15f1eeed2d7a981e849f7e91e0872b9b59ced7
Bozhidar Batsov <bozhidar.batsov@gmail.com>
2011-10-08 23:17:03 +0300

    fixed list of required packages

--- prelude-packages.el
+++ prelude-packages.el
@@ -41,15 +41,15 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(defvar prelude-packages '(auctex clojure-mode coffee-mode gist haml-mode
+(defvar prelude-packages '(clojure-mode coffee-mode gist haml-mode
                                   haskell-mode magit markdown-mode paredit
-                                  sass-mode yaml yari yasnippet)
+                                  sass-mode scss-mode yaml-mode yari yasnippet)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p prelude-packages)
   (when (not (package-installed-p p))
     (package-install p)))
 
-(provide 'packages-prelude)
+(provide 'prelude-packages)
 
 ;;; prelude-packages.el ends here
