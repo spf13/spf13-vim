6b6864f0fbc8897eaa9eea22fc9b53de231dfbbf
Steve Francia <steve.francia@gmail.com>
2011-11-19 23:43:15 -0500

    Few improvements to the bootstrap file.
    Now backups of existing .vim & .vimrc files aren't overwritten if a
    backup already existed (backup extension is todays date).

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/env sh
 
-endpath="$HOME/.spf13-vim"
+endpath="$HOME/.spf13-vim-3"
 
 warn() {
     echo "$1" >&2
@@ -15,11 +15,11 @@ echo "thanks for installing spf13-vim\n"
 
 # Backup existing .vim stuff
 echo "backing up current vim config\n"
-for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
+today=`date +%Y%m%d`
+for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.$today; done
 
 
 echo "cloning spf13-vim\n"
-#git clone --recursive git://github.com/spf13/spf13-vim.git ~/.spf13-vim 
 git clone --recursive -b 3.0 git://github.com/spf13/spf13-vim.git $endpath
 ln -s $endpath/.vimrc ~/.vimrc
 ln -s $endpath/.vim ~/.vim
@@ -30,5 +30,5 @@ vim +BundleInstall! +BundleClean +q
 # Build command-t for your system
 echo "building command-t executable\n"
 echo "command-t depends on ruby and rake to be present\n"
-cd ~/.vim/bundle/Command-t
+cd $HOME/.vim/bundle/Command-t
 (rake make) || warn "Ruby compilation failed. Ruby, GCC or rake not installed?"
