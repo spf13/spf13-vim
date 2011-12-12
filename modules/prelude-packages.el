c6213b77bb61bd59694127229552dce7340f0327
Bozhidar Batsov <bozhidar@empowerunited.com>
2011-12-12 15:45:23 +0200

    inf-ruby 2.2.3 is finally in Marmalade, no need to keep it under vendor

--- modules/prelude-packages.el
+++ modules/prelude-packages.el
@@ -43,10 +43,9 @@
 (package-refresh-contents)
 (message "%s" " done.")
 
-;; TODO find a way to get inf-ruby 2.2.2 into Marmalade
 (defvar prelude-packages
   '(auctex clojure-mode coffee-mode deft gist haml-mode
-           haskell-mode markdown-mode paredit projectile
+           haskell-mode inf-ruby markdown-mode paredit projectile
            python sass-mode scss-mode solarized-theme yaml-mode yari zenburn-theme)
   "A list of packages to ensure are installed at launch.")
 
