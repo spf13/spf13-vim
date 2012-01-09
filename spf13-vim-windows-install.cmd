c89a7bbb7527d291f3c4aed6d6838536d830591a
Steve Francia <steve.francia@gmail.com>
2012-01-09 15:03:39 -0500

    Adding a new Windows install script

---
+++ spf13-vim-windows-install.cmd
@@ -0,0 +1,11 @@
+@if not exist "%HOME%" @set HOME=%HOMEDRIVE%%HOMEPATH%
+@if not exist "%HOME%" @set HOME=%USERPROFILE%
+
+@set BASE_DIR=%HOME%\.spf13-vim-3
+call git clone --recursive -b 3.0 git://github.com/spf13/spf13-vim.git %BASE_DIR%
+call mkdir %BASE_DIR%\.vim\bundle
+call mklink /J %HOME%\.vim %BASE_DIR%\.vim
+call mklink %HOME%\.vimrc %BASE_DIR%\.vimrc
+call mklink %HOME%\_vimrc %BASE_DIR%\.vimrc
+
+call git clone http://github.com/gmarik/vundle.git %HOME%/.vim/bundle/vundle
