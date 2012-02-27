cc2836c4f6d4e439d84200d4fc65078ae4b1c1c7
Perry Kundert <perry@kundert.ca>
2012-02-27 12:49:00 -0700

    Retain correct 8-character tabs, but avoid them for future indenting

--- modules/prelude-editor.el
+++ modules/prelude-editor.el
@@ -43,8 +43,18 @@
   (setq mac-command-modifier 'super)
   (setq mac-option-modifier 'meta))
 
-;; Death to the tabs!
-(setq-default indent-tabs-mode nil)
+;; Death to the tabs!  However, tabs historically indent to the next
+;; 8-character offset; specifying anything else will cause *mass*
+;; confusion, as it will change the appearance of every existing file.
+;; In some cases (python), even worse -- it will change the semantics
+;; (meaning) of the program.
+;;
+;; Emacs modes typically provide a standard means to change the
+;; indentation width -- eg. c-basic-offset: use that to adjust your
+;; personal indentation width, while maintaining the style (and
+;; meaning) of any files you load.
+(setq-default indent-tabs-mode nil)   ;; don't use tabs to indent
+(setq-default tab-width 8)            ;; but maintain correct appearance
 
 ;; delete the selection with a keypress
 (delete-selection-mode t)
