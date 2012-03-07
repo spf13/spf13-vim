5f81a0bdd8f4b544b5e91248091bf09e927fca47
Bozhidar Batsov <bozhidar@empowerunited.com>
2012-03-07 11:51:57 +0200

    moved the auto-save code to the editor module

--- modules/prelude-editor.el
+++ modules/prelude-editor.el
@@ -126,6 +126,21 @@
 (require 'windmove)
 (windmove-default-keybindings 'super)
 
+;; automatically save buffers associated with files on buffer switch
+;; and on windows switch
+(defadvice switch-to-buffer (before save-buffer-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice other-window (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-up (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-down (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-left (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-right (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+
 ;; show-paren-mode: subtle highlighting of matching parens
 (show-paren-mode t)
 (setq show-paren-style 'parenthesis)
