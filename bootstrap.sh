8b2c08c71e7b117436a589f3d19aff0c04392ef7
Steve Francia <steve.francia@gmail.com>
2012-09-27 18:06:24 -0400

    fixing some of the bootstrap issues

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,20 +24,28 @@ echo "backing up current vim config\n"
 today=`date +%Y%m%d`
 for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && [ ! -L $file ] && mv $i $i.$today; done
 
-mkdir -p $endpath/.vim/bundle
-lnif $endpath/.vimrc $HOME/.vimrc
-lnif $endpath/.vimrc.bundles $HOME/.vimrc.bundles
-lnif $endpath/.vim $HOME/.vim
 
-if [ ! -d $endpath ]; then
+if [ ! -e $endpath/.git ]; then
     echo "cloning spf13-vim\n"
     git clone --recursive -b 3.0 http://github.com/spf13/spf13-vim.git $endpath
-    echo "Installing Vundle"
-    git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
 else
     echo "updating spf13-vim\n"
     cd $endpath && git pull
 fi
 
-echo "installing plugins using Vundle"
+
+echo "setting up symlinks"
+lnif $endpath/.vimrc $HOME/.vimrc
+lnif $endpath/.vimrc.bundles $HOME/.vimrc.bundles
+lnif $endpath/.vim $HOME/.vim
+if [ ! -d $endpath/.vim/bundle ]; then
+    mkdir -p $endpath/.vim/bundle
+fi
+
+if [ ! -e $HOME/.vim/bundle/vundle ]; then
+    echo "Installing Vundle"
+    git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+fi
+
+echo "update/install plugins using Vundle"
 vim -u $endpath/.vimrc.bundles - +BundleInstall! +BundleClean +qall
