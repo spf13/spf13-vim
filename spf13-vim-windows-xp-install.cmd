b72c6811d4a29f196093c5fba53419e1e19b3547
Jan Wąsak <jhnwsk@gmail.com>
2012-10-17 14:28:09 +0200

    add update functionality to xp-installer

--- spf13-vim-windows-xp-install.cmd
+++ spf13-vim-windows-xp-install.cmd
@@ -12,9 +12,18 @@
         @if exist "%HOME%\.gvimrc" call copy "%HOME%\.gvimrc" "%HOME%\.gvimrc.%today%"
         )
 
-call git clone --recursive -b 3.0 git://github.com/spf13/spf13-vim.git "%BASE_DIR%"
-call mkdir "%BASE_DIR%\.vim\bundle"
-call copy "%BASE_DIR%\.vim" "%HOME%\.vim"
+@if exist "%BASE_DIR%" (
+        @set ORIGINAL_DIR=%CD%
+        echo updating spf13-vim
+        chdir /d "%BASE_DIR%" && git pull 
+        chdir /d "%ORIGINAL_DIR%"
+        ) else (
+            echo cloning spf13-vim
+            call git clone --recursive -b 3.0 git://github.com/spf13/spf13-vim.git "%BASE_DIR%"
+        )
+
+@if not exist  "%BASE_DIR%\.vim\bundle" call mkdir "%BASE_DIR%\.vim\bundle"
+call xcopy /s/e/h/y/r/q/i "%BASE_DIR%\.vim" "%HOME%\.vim"
 call copy "%BASE_DIR%\.vimrc" "%HOME%\.vimrc"
 call copy "%BASE_DIR%\.vimrc" "%HOME%\_vimrc"
 call copy "%BASE_DIR%\.vimrc.bundles" "%HOME%\.vimrc.bundles"
