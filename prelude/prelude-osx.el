618a4627dce32cccc2b70688762a24b0641ffb01
Bozhidar Batsov <bozhidar@tradeo.com>
2012-10-21 09:23:09 +0300

    added the exec-path-from-shell package

--- prelude/prelude-osx.el
+++ prelude/prelude-osx.el
@@ -1,11 +1,6 @@
 ;; On OS X Emacs doesn't use the shell PATH if it's not started from
-;; the shell.
-;; If you're using homebrew or port, modifying the PATH is essential.
-(let (osx-paths)
-  (dolist (path '("/usr/local/bin" "/opt/local/bin" "/opt/local/sbin" "$HOME/bin")
-                (setenv "PATH" (concat osx-paths (getenv "PATH"))))
-    (push path exec-path)
-    (setq osx-paths (concat (concat path ":") osx-paths))))
+;; the shell. Let's fix that:
+(exec-path-from-shell-initialize)
 
 ;; Emacs users obviously have little need for Command and Option keys,
 ;; but they do need Meta and Super
