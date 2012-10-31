5f11f8ddebcbdd4bca023cd3f746730ec5ef119d
Chris Raschl <cr@kautsig.org>
2012-10-31 08:24:46 +0100

    fixed helm-mini fallback

--- prelude/prelude-editor.el
+++ prelude/prelude-editor.el
@@ -219,16 +219,19 @@
 (defun helm-prelude ()
   "Preconfigured `helm'."
   (interactive)
-  (if (projectile-project-root)
-      ;; add project files and buffers when in project
-      (helm-other-buffer '(helm-c-source-projectile-files-list
-                           helm-c-source-projectile-buffers-list
-                           helm-c-source-buffers-list
-                           helm-c-source-recentf
-                           helm-c-source-buffer-not-found)
-                         "*helm prelude*")
-    ;; otherwise fallback to helm-mini
-    (helm-mini)))
+  (condition-case nil
+    (if (projectile-project-root)
+        ;; add project files and buffers when in project
+        (helm-other-buffer '(helm-c-source-projectile-files-list
+                             helm-c-source-projectile-buffers-list
+                             helm-c-source-buffers-list
+                             helm-c-source-recentf
+                             helm-c-source-buffer-not-found)
+                           "*helm prelude*")
+      ;; otherwise fallback to helm-mini
+      (helm-mini))
+    ;; fall back to helm mini if an error occurs (usually in projectile-project-root)
+    (error (helm-mini))))
 
 ;; shorter aliases for ack-and-a-half commands
 (defalias 'ack 'ack-and-a-half)
