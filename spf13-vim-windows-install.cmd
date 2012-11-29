0ec8486c46083a2f9b3abd8c69187a08abb63e78
Raahul Seshadri <raahul@musictypes.com>
2012-11-29 13:39:25 +0530

    Added quotes to external command calls
    The quotes are needed if the username has spaces, which can be common on
    Windows environment.

--- spf13-vim-windows-install.cmd
+++ spf13-vim-windows-install.cmd
@@ -2,12 +2,12 @@
 @if not exist "%HOME%" @set HOME=%USERPROFILE%
 
 @set BASE_DIR=%HOME%\.spf13-vim-3
-call git clone --recursive -b 3.0 git://github.com/spf13/spf13-vim.git %BASE_DIR%
-call mkdir %BASE_DIR%\.vim\bundle
-call mklink /J %HOME%\.vim %BASE_DIR%\.vim
-call mklink %HOME%\.vimrc %BASE_DIR%\.vimrc
-call mklink %HOME%\_vimrc %BASE_DIR%\.vimrc
-call mklink %HOME%\.vimrc.bundles %BASE_DIR%\.vimrc.bundles
+call git clone --recursive -b 3.0 git://github.com/spf13/spf13-vim.git "%BASE_DIR%"
+call mkdir "%BASE_DIR%\.vim\bundle"
+call mklink /J "%HOME%\.vim" "%BASE_DIR%\.vim"
+call mklink "%HOME%\.vimrc" "%BASE_DIR%\.vimrc"
+call mklink "%HOME%\_vimrc" "%BASE_DIR%\.vimrc"
+call mklink "%HOME%\.vimrc.bundles" "%BASE_DIR%\.vimrc.bundles"
 
-call git clone http://github.com/gmarik/vundle.git %HOME%/.vim/bundle/vundle
+call git clone http://github.com/gmarik/vundle.git "%HOME%/.vim/bundle/vundle"
 call vim -u "$BASE_DIR/.vimrc.bundles" - +BundleInstall! +BundleClean +qall
