5a14cdb48636399620aec5d48b58895a730d25f2
Bozhidar Batsov <bozhidar@tradeo.com>
2013-03-12 21:09:05 +0200

    Display a message when loading the personal config

--- init.el
+++ init.el
@@ -101,6 +101,7 @@ by Prelude.")
 
 ;; load the personal settings (this includes `custom-file')
 (when (file-exists-p prelude-personal-dir)
+  (message "Loading personal configuration files in %s..." prelude-personal-dir)
   (mapc 'load (directory-files prelude-personal-dir 't "^[^#].*el$")))
 
 (message "Prelude is ready to do thy bidding, Master %s!" (getenv "USER"))
