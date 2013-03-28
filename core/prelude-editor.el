38959dd81b582d6b77ab2db638ba90d1382547ec
Bozhidar Batsov <bozhidar@tradeo.com>
2013-03-28 17:30:03 +0200

    Code style

--- core/prelude-editor.el
+++ core/prelude-editor.el
@@ -54,7 +54,8 @@
   :group 'prelude)
 
 (defcustom prelude-clean-whitespace-on-save t
-  "Cleanup whitespace from file before it's saved.  Will only occur if prelude-whitespace is also enabled"
+  "Cleanup whitespace from file before it's saved.
+Will only occur if prelude-whitespace is also enabled."
   :type 'boolean
   :group 'prelude)
 
