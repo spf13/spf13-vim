1f8baf08ebd1108d72ba507a75f7d1b37a7ff87b
Bozhidar Batsov <bozhidar@tradeo.com>
2013-04-30 23:39:54 +0300

    Enhance a bit stock dired config

--- core/prelude-editor.el
+++ core/prelude-editor.el
@@ -314,6 +314,14 @@ Will only occur if prelude-whitespace is also enabled."
 ;; dired - reuse current buffer by pressing 'a'
 (put 'dired-find-alternate-file 'disabled nil)
 
+;; always delete and copy recursively
+(setq dired-recursive-deletes 'always)
+(setq dired-recursive-copies 'always)
+
+;; if there is a dired buffer displayed in the next window, use its
+;; current subdir, instead of the current subdir of this dired buffer
+(setq dired-dwim-target t)
+
 ;; enable some really cool extensions like C-x C-j(dired-jump)
 (require 'dired-x)
 
