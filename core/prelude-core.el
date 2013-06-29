f76aab7491b98ac91766249df555fb0532d25716
Bozhidar Batsov <bozhidar@tradeo.com>
2013-06-29 09:26:23 +0300

    Add prefix arg to prelude-smart-open-line
    With a prefix arg the command will invoke prelude-smart-open-line-above

--- core/prelude-core.el
+++ core/prelude-core.el
@@ -90,12 +90,17 @@ Position the cursor at it's beginning, according to the current mode."
   (forward-line -1)
   (indent-according-to-mode))
 
-(defun prelude-smart-open-line ()
+(defun prelude-smart-open-line (arg)
   "Insert an empty line after the current line.
-Position the cursor at its beginning, according to the current mode."
-  (interactive)
-  (move-end-of-line nil)
-  (newline-and-indent))
+Position the cursor at its beginning, according to the current mode.
+
+With a prefix ARG open line above the current line."
+  (interactive "P")
+  (if arg
+      (prelude-smart-open-line-above)
+    (progn
+      (move-end-of-line nil)
+      (newline-and-indent))))
 
 (defun prelude-top-join-line ()
   "Join the current line with the line beneath it."
