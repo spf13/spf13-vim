5b6bef1d6790a66d943f102d7be378746b74e60b
Bozhidar Batsov <bozhidar@tradeo.com>
2013-06-29 15:49:03 +0300

    Don't create the snippets dir

--- utils/installer.sh
+++ utils/installer.sh
@@ -16,7 +16,7 @@ install_prelude () {
 make_prelude_dirs () {
     printf " Making the required directories.\n$RESET"
     mkdir -p "$PRELUDE_INSTALL_DIR/vendor" "$PRELUDE_INSTALL_DIR/personal"
-    mkdir -p "$PRELUDE_INSTALL_DIR/themes" "$PRELUDE_INSTALL_DIR/snippets"
+    mkdir -p "$PRELUDE_INSTALL_DIR/themes"
     mkdir -p "$PRELUDE_INSTALL_DIR/savefile"
 }
 
