daabb1e3732d45b3925d638ee6e2e7c74cdf317c
Bozhidar Batsov <bozhidar@tradeo.com>
2013-07-11 11:23:15 +0300

    Use helm-projectile directly (fixes #352)

--- core/prelude-editor.el
+++ core/prelude-editor.el
@@ -289,13 +289,7 @@ Will only occur if prelude-whitespace is also enabled."
   (interactive)
   (condition-case nil
     (if (projectile-project-root)
-        ;; add project files and buffers when in project
-        (helm-other-buffer '(helm-c-source-projectile-files-list
-                             helm-c-source-projectile-buffers-list
-                             helm-c-source-buffers-list
-                             helm-c-source-recentf
-                             helm-c-source-buffer-not-found)
-                           "*helm prelude*")
+        (helm-projectile)
       ;; otherwise fallback to helm-mini
       (helm-mini))
     ;; fall back to helm mini if an error occurs (usually in projectile-project-root)
