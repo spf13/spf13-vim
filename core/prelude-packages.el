07c1118a22b1a267190833e379f730e6caa80b7e
Bozhidar Batsov <bozhidar@batsov.com>
2013-07-17 00:17:13 +0300

    Add flx-ido to the list of default packages

--- core/prelude-packages.el
+++ core/prelude-packages.el
@@ -42,7 +42,7 @@
 
 (defvar prelude-packages
   '(ace-jump-mode ack-and-a-half dash diminish elisp-slime-nav
-    expand-region flx flycheck gist
+    expand-region flx-ido flycheck gist
     git-commit-mode gitconfig-mode gitignore-mode grizzl
     guru-mode helm helm-projectile ido-ubiquitous
     key-chord magit rainbow-mode
