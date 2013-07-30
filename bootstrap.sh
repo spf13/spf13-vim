f4ef06088c5142c2d6ecfe1d934faa78bd257778
spf13 <steve.francia@gmail.com>
2013-07-29 22:25:12 -0400

    making new installer posix compliant

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,54 +1,53 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ############################  SETUP PARAMETERS
-required=('vim' 'git' 'vundle');
 app_name='spf13-vim'
 git_branch='3.0'
 debug_mode='0'
 fork_maintainer='0'
 
 ############################  BASIC SETUP TOOLS
-function msg() {
+msg() {
     printf '%b\n' "$1" >&2
 }
 
-function success {
-    if [[ "$ret" -eq '0' ]]; then
-	msg "\e[32m[✔]\033[0m ${1}${2}"
+success() {
+    if [ "$ret" -eq '0' ]; then
+    msg "\e[32m[✔]\033[0m ${1}${2}"
     fi
 }
 
-function error() {
+error() {
     msg "\e[31m[✘]\033[0m ${1}${2}"
     exit 1
 }
 
-function debug() {
-    if [[ "$debug_mode" -eq '1' && "$ret" -gt '1' ]]; then
+debug() {
+    if [ "$debug_mode" -eq '1' ] && [ "$ret" -gt '1' ]; then
       msg "An error occured in function \"${FUNCNAME[$i+1]}\" on line ${BASH_LINENO[$i+1]}, we're sorry for that."
     fi
 }
 
-function program_exists {
+program_exists() {
     local ret='0'
     type $1 >/dev/null 2>&1 || { local ret='1'; }
 
     # throw error on non-zero return value
-    if [[ ! "$ret" -eq '0' ]]; then
-	error "$2"
+    if [ ! "$ret" -eq '0' ]; then
+    error "$2"
     fi
 }
 
 ############################ SETUP FUNCTIONS
-function lnif() {
-    if [[ -e "$1" ]]; then
+lnif() {
+    if [ -e "$1" ]; then
         ln -sf "$1" "$2"
     fi
     ret="$?"
     debug
 }
 
-function do_backup() {
-    if [[ -e "$2" || -e "$3" || -e "$4" ]]; then
+do_backup() {
+    if [ -e "$2" ] || [ -e "$3" ] || [ -e "$4" ]; then
         today=`date +%Y%m%d_%s`
         for i in "$2" "$3" "$4"; do
             [ -e "$i" ] && [ ! -L "$i" ] && mv "$i" "$i.$today";
@@ -59,17 +58,17 @@ function do_backup() {
    fi
 }
 
-function upgrade_repo() {
+upgrade_repo() {
       msg "trying to update $1"
 
-      if [[ "$1" == "$app_name" ]]; then
-	  cd "$HOME/.$app_name-3" &&
-	  git pull origin "$git_branch"
+      if [ "$1" = "$app_name" ]; then
+          cd "$HOME/.$app_name-3" &&
+          git pull origin "$git_branch"
       fi
 
-      if [[ "$1" == "${required[2]}" ]]; then
-	  cd "$HOME/.vim/bundle/vundle" &&
-	  git pull origin master
+      if [ "$1" = "vundle" ]; then
+          cd "$HOME/.vim/bundle/vundle" &&
+          git pull origin master
       fi
 
       ret="$?"
@@ -77,54 +76,54 @@ function upgrade_repo() {
       debug
 }
 
-function clone_repo() {
-    program_exists "${required[1]}" "Sorry, we cannot continue without GIT, please install it first."
+clone_repo() {
+    program_exists "git" "Sorry, we cannot continue without GIT, please install it first."
     endpath="$HOME/.$app_name-3"
 
     if [ ! -e "$endpath/.git" ]; then
-	git clone --recursive -b "$git_branch" https://github.com/spf13/spf13-vim.git "$endpath"
-	ret="$?"
-	success "$1"
-	debug
+        git clone --recursive -b "$git_branch" https://github.com/spf13/spf13-vim.git "$endpath"
+        ret="$?"
+        success "$1"
+        debug
     else
-	upgrade_repo "$app_name"	"Successfully updated $app_name"
+        upgrade_repo "$app_name"    "Successfully updated $app_name"
     fi
 }
 
-function clone_vundle() {
-    if [[ ! -e "$HOME/.vim/bundle/vundle" ]]; then
-	git clone https://github.com/gmarik/vundle.git "$HOME/.vim/bundle/vundle"
+clone_vundle() {
+    if [ ! -e "$HOME/.vim/bundle/vundle" ]; then
+        git clone https://github.com/gmarik/vundle.git "$HOME/.vim/bundle/vundle"
     else
-	upgrade_repo "${required[2]}"	"Successfully updated ${required[2]}"
+        upgrade_repo "vundle"   "Successfully updated vundle"
     fi
     ret="$?"
     success "$1"
     debug
 }
 
-function create_symlinks() {
+create_symlinks() {
     endpath="$HOME/.$app_name-3"
 
-    ln -sf "$endpath/.vimrc"              "$HOME/.vimrc"
-    ln -sf "$endpath/.vimrc.bundles"      "$HOME/.vimrc.bundles"
-    ln -sf "$endpath/.vim"                "$HOME/.vim"
+    lnif "$endpath/.vimrc"              "$HOME/.vimrc"
+    lnif "$endpath/.vimrc.bundles"      "$HOME/.vimrc.bundles"
+    lnif "$endpath/.vim"                "$HOME/.vim"
 
     # Useful for fork maintainers
     touch  "$HOME/.vimrc.local"
 
-    if [[ -e "$endpath/.vimrc.fork" ]]; then
-        ln -sf "$HOME/.vimrc.fork"
-    elif [[ "$fork_maintainer" -eq '1' ]]; then
+    if [ -e "$endpath/.vimrc.fork" ]; then
+        ln -sf "$endpath/.vimrc.fork" "$HOME/.vimrc.fork"
+    elif [ "$fork_maintainer" -eq '1' ]; then
        touch "$HOME/.vimrc.fork"
        touch "$HOME/.vimrc.bundles.fork"
     fi
 
-    if [[ -e "$endpath/.vimrc.bundles.fork" ]]; then
-        ln -sf "$HOME/.vimrc.bundles.fork"
+    if [ -e "$endpath/.vimrc.bundles.fork" ]; then
+        ln -sf "$endpath/.vimrc.bundles.fork" "$HOME/.vimrc.bundles.fork"
     fi
 
-    if [[ ! -d "$endpath/.vim/bundle" ]]; then
-	mkdir -p "$endpath/.vim/bundle"
+    if [ ! -d "$endpath/.vim/bundle" ]; then
+        mkdir -p "$endpath/.vim/bundle"
     fi
 
     ret="$?"
@@ -132,7 +131,7 @@ function create_symlinks() {
     debug
 }
 
-function setup_vundle() {
+setup_vundle() {
     system_shell="$SHELL"
     export SHELL='/bin/sh'
     vim -u "$HOME/.vimrc.bundles" +BundleInstall! +BundleClean +qall
@@ -143,20 +142,20 @@ function setup_vundle() {
 }
 
 ############################ MAIN()
-program_exists "${required[0]}" "To install $app_name you first need to install Vim."
+program_exists "vim" "To install $app_name you first need to install Vim."
 
-do_backup	"Your old vim stuff has a suffix now and looks like .vim.`date +%Y%m%d_%s`" \
-		"$HOME/.vim" \
-		"$HOME/.vimrc" \
-		"$HOME/.gvimrc"
+do_backup   "Your old vim stuff has a suffix now and looks like .vim.`date +%Y%m%d_%s`" \
+        "$HOME/.vim" \
+        "$HOME/.vimrc" \
+        "$HOME/.gvimrc"
 
-clone_repo 	"Successfully cloned $app_name"
-
-clone_vundle 	"Successfully cloned ${required[2]}"
+clone_repo  "Successfully cloned $app_name"
 
 create_symlinks "Setting up vim symlinks"
 
-setup_vundle 	"Now updating/installing plugins using Vundle"
+clone_vundle    "Successfully cloned vundle"
 
-msg 		"\nThanks for installing $app_name."
-msg		"© `date +%Y` http://vim.spf13.com/"
+setup_vundle    "Now updating/installing plugins using Vundle"
+
+msg         "\nThanks for installing $app_name."
+msg     "© `date +%Y` http://vim.spf13.com/"
