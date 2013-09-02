09635ea8704e920d31ec02b14435815584eb6d0a
Bozhidar Batsov <bozhidar@tradeo.com>
2013-09-02 18:38:04 +0300

    [Fix #378] Add folders recursively to the load-path in prelude-add-subfolders-to-load-path

--- init.el
+++ init.el
@@ -61,13 +61,14 @@ by Prelude.")
   (make-directory prelude-savefile-dir))
 
 (defun prelude-add-subfolders-to-load-path (parent-dir)
- "Add all first level PARENT-DIR subdirs to the `load-path'."
+ "Add all level PARENT-DIR subdirs to the `load-path'."
  (dolist (f (directory-files parent-dir))
    (let ((name (expand-file-name f parent-dir)))
      (when (and (file-directory-p name)
                 (not (equal f ".."))
                 (not (equal f ".")))
-       (add-to-list 'load-path name)))))
+       (add-to-list 'load-path name)
+       (prelude-add-subfolders-to-load-path name)))))
 
 ;; add Prelude's directories to Emacs's `load-path'
 (add-to-list 'load-path prelude-core-dir)
