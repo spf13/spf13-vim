78b6a8083d16ac1c7ce56ff71c61114ae4671bf2
Bozhidar Batsov <bozhidar@tradeo.com>
2013-09-18 12:17:51 +0300

    Enable smartparens-strict-mode in Lisps

--- modules/prelude-lisp.el
+++ modules/prelude-lisp.el
@@ -40,12 +40,14 @@
 
 ;; a great lisp coding hook
 (defun prelude-lisp-coding-defaults ()
+  (smartparens-strict-mode +1)
   (rainbow-delimiters-mode +1))
 
 (setq prelude-lisp-coding-hook 'prelude-lisp-coding-defaults)
 
 ;; interactive modes don't need whitespace checks
 (defun prelude-interactive-lisp-coding-defaults ()
+  (smartparens-strict-mode +1)
   (rainbow-delimiters-mode +1)
   (whitespace-mode -1))
 
