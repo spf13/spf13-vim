b3277d190e7438692a727451d6b1b9251d244fea
Bozhidar Batsov <bozhidar@tradeo.com>
2013-09-26 14:34:10 +0300

    Add a comment

--- modules/prelude-programming.el
+++ modules/prelude-programming.el
@@ -130,6 +130,7 @@ This functions should be added to the hooks of major modes for programming."
 (add-hook 'prog-mode-hook (lambda ()
                             (run-hooks 'prelude-prog-mode-hook)))
 
+;; enable on-the-fly syntax checking
 (if (fboundp 'global-flycheck-mode)
     (global-flycheck-mode +1)
   (add-hook 'prog-mode-hook 'flycheck-mode))
