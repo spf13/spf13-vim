99a56d8204a2319a66eddc3e54ffe4622400a5c3
Johnny Robeson <johnny@localmomentum.net>
2014-06-27 03:05:22 -0400

    BC BREAK: rename git_uri to REPO_URI

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,7 +17,7 @@
 ############################  SETUP PARAMETERS
 app_name='spf13-vim'
 [ -z "$APP_PATH"] && APP_PATH="$HOME/.spf13-vim-3"
-[ -z "$git_uri" ] && git_uri='https://github.com/spf13/spf13-vim.git'
+[ -z "$REPO_URI" ] && REPO_URI='https://github.com/spf13/spf13-vim.git'
 [ -z "$REPO_BRANCH" ] && REPO_BRANCH='3.0'
 debug_mode='0'
 fork_maintainer='0'
@@ -171,7 +171,7 @@ do_backup       "$HOME/.vim" \
                 "$HOME/.gvimrc"
 
 sync_repo       "$APP_PATH" \
-                "$git_uri" \
+                "$REPO_URI" \
                 "$REPO_BRANCH" \
                 "$app_name"
 
