c5ca895e3bf8183b01d8f980e3f239c973f26ec8
Jan Viljanen <jan.viljanen@greenpeace.org>
2016-01-03 10:25:07 +0100

    Remove redundant code

--- bootstrap.sh
+++ bootstrap.sh
@@ -45,25 +45,9 @@ debug() {
     fi
 }
 
-program_exists() {
-    local ret='0'
-    command -v $1 >/dev/null 2>&1 || { local ret='1'; }
-
-    # fail on non-zero return value
-    if [ "$ret" -ne 0 ]; then
-        return 1
-    fi
-
-    return 0
-}
-
 program_must_exist() {
-    program_exists $1
-
-    # throw error on non-zero return value
-    if [ "$?" -ne 0 ]; then
-        error "You must have '$1' installed to continue."
-    fi
+    # Check if program exists else exit with an error
+    command -v $1 >/dev/null 2>&1 || { error "You must have '$1' installed to continue."; }
 }
 
 variable_set() {
