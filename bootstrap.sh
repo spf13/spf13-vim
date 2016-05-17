74f07099ecb6fa6dcecffa13dc4d7dbd77f22065
Luis Alejandro Martínez Faneyth <luis@huntingbears.com.ve>
2016-05-17 00:07:26 -0400

    [FIX] There has been a change on the default branch, so we need to be specific.

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,7 +17,7 @@ for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
 
 
 echo "cloning spf13-vim\n"
-git clone --recursive git://github.com/spf13/spf13-vim.git ~/.spf13-vim 
+git clone -b master --recursive git://github.com/spf13/spf13-vim.git ~/.spf13-vim
 ln -s ~/.spf13-vim/.vimrc ~/.vimrc
 ln -s ~/.spf13-vim/.vim ~/.vim
 
