1a8753df7a100d249f1d74ad0413f48ec8ac18c6
leoatchina <leoatchina@gmail.com>
2016-12-26 15:51:58 +0800

    change windows install files

--- spf13-leoatchina-windows-install.cmd
+++ spf13-leoatchina-windows-install.cmd
@@ -16,29 +16,17 @@ REM    limitations under the License.
 @if not exist "%HOME%" @set HOME=%HOMEDRIVE%%HOMEPATH%
 @if not exist "%HOME%" @set HOME=%USERPROFILE%
 
-@set APP_PATH=%cd%
-IF NOT EXIST "%APP_PATH%" (
-    call git clone -b 3.0 https://github.com/leoatchina/spf13-vim-leoatchina.git "%APP_PATH%"
-) ELSE (
-    @set ORIGINAL_DIR=%CD%
-    echo updating spf13-vim
-    chdir /d "%APP_PATH%"
-    call git pull
-    chdir /d "%ORIGINAL_DIR%"
-    call cd "%APP_PATH%"
-)
+@set APP_PATH=%~dp0
+echo "%APP_PATH%"
+call mklink "%HOME%/.vimrc" "%APP_PATH%/.vimrc"
+call mklink "%HOME%/_vimrc" "%APP_PATH%/.vimrc"
+call mklink "%HOME%/.vimrc.bundles" "%APP_PATH%/.vimrc.bundles"
+call mklink "%HOME%/.vimrc.before" "%APP_PATH%/.vimrc.before"
 
-call mklink "%HOME%\.vimrc" "%APP_PATH%\.vimrc"
-call mklink "%HOME%\_vimrc" "%APP_PATH%\.vimrc"
-call mklink "%HOME%\.vimrc.fork" "%APP_PATH%\.vimrc.fork"
-call mklink "%HOME%\.vimrc.bundles" "%APP_PATH%\.vimrc.bundles"
-call mklink "%HOME%\.vimrc.bundles.fork" "%APP_PATH%\.vimrc.bundles.fork"
-call mklink "%HOME%\.vimrc.before" "%APP_PATH%\.vimrc.before"
-call mklink "%HOME%\.vimrc.before.fork" "%APP_PATH%\.vimrc.before.fork"
-call mklink /J "%HOME%\.vim" "%APP_PATH%\.vim"
+call mkdir "%HOME%/.vim"
 
-IF NOT EXIST "%APP_PATH%\.vim\bundle" (
-    call mkdir "%APP_PATH%\.vim\bundle"
+IF NOT EXIST "%HOME%/.vim/bundle" (
+    call mkdir "%HOME%/.vim/bundle"
 )
 
 IF NOT EXIST "%HOME%/.vim/bundle/vundle" (
@@ -49,4 +37,4 @@ IF NOT EXIST "%HOME%/.vim/bundle/vundle" (
   call cd %HOME%
 )
 
-call vim -u "%APP_PATH%/.vimrc.bundles" +BundleInstall! +BundleClean +qall
+call vim -u "%HOME%/.vimrc.bundles" +BundleInstall! +BundleClean +qall
