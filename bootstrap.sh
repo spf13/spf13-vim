4fd048e63e530259ae3ef4d88fd7cd942781b2b7
Chengsheng Shen <shenchsh94@gmail.com>
2017-07-29 18:25:06 -0700

    Set fork mode

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,7 +20,7 @@ app_name='chanson-vim'
 [ -z "$REPO_URI" ] && REPO_URI='https://github.com/shenchsh/chanson-vim.git'
 [ -z "$REPO_BRANCH" ] && REPO_BRANCH='3.0'
 debug_mode='0'
-fork_maintainer='0'
+fork_maintainer='1'
 [ -z "$VUNDLE_URI" ] && VUNDLE_URI="https://github.com/gmarik/vundle.git"
 
 ############################  BASIC SETUP TOOLS
